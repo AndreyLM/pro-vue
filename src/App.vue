5eb021ec6aedaa16480cc21608da15d1ef52eff3
Andrew <kpi_lithium@ukr.net>
2019-05-31 19:27:27 +0300

    templs

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-primary text-white text-center m-2 p-3">
+    <h3>Product: {{ name | reverse | capitalize }}</h3>
+    <h3>Price: {{ getTotalPrice(lowTaxRate) | currency(3) }} (Low Rate)</h3>
+    <h3>Price: {{ getTotalPrice(highTaxRate) | currency }} (Hight Rate)</h3>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: 'MyComponent',
+  data: function() {
+    return {
+      name: "Kayak",
+      price: 275,
+      lowTaxRate: 12,
+      highTaxRate: 20
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+    getTotalPrice(taxRate) {
+      return this.price + this.price*taxRate/100;
+    }
+  },
+  filters: {
+    currency(value, places) {
+      return new Intl.NumberFormat("ua-UA", { 
+          style: "currency", 
+          currency: "GBP",
+          minimumFractionDigits: places || 2,
+          maximumFractionDigits: places || 2 
+        }).format(value);
+    },
+    reverse(val) {
+      return val.split("").reverse().join("");
+    },
+    capitalize(val) {
+      return val[0].toUpperCase()+val.slice(1);
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
